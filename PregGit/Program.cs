9a56b949d04cf66d383520e89b6c0fba958893ed
Eduard <edurisub@gmail.com>
2021-03-08 13:24:12 +0100

    R2Versio1

--- PregGit/Program.cs
+++ PregGit/Program.cs
@@ -52,7 +52,137 @@ namespace PregGit
 
         static void Tema2()
         {
+            char rpreg;
+            int r = 0;
 
+            Console.Clear();
+            Console.WriteLine("REDSOCK");
+            Console.WriteLine("1- Quin any es va fundar RedSock?");
+            Console.WriteLine("1. 2013");
+            Console.WriteLine("2. 1972");
+            Console.WriteLine("3. 2019");
+            Console.WriteLine("4. 2020");
+
+            Console.WriteLine("Introdueix el valor: ");
+            rpreg = Convert.ToChar(Console.ReadKey().KeyChar);
+
+            switch (rpreg)
+            {
+                case '1':
+                    r = r;
+                    break;
+                case '2':
+                    r = r;
+                    break;
+                case '3':
+                    r++;
+                    break;
+                case '4':
+                    r = r;
+                    break;
+            }
+
+
+            Console.WriteLine("2- Qui es el principal fundador?");
+            Console.WriteLine("1. Eduard Ribas");
+            Console.WriteLine("2. Cristian Reyes");
+            Console.WriteLine("3. Biel Auguet");
+            Console.WriteLine("4. Pedro Sar");
+
+            Console.WriteLine("Introdueix el valor: ");
+            rpreg = Convert.ToChar(Console.ReadKey().KeyChar);
+
+            switch (rpreg)
+            {
+                case '1':
+                    r++;
+                    break;
+                case '2':
+                    r = r;
+                    break;
+                case '3':
+                    r = r;
+                    break;
+                case '4':
+                    r = r;
+                    break;
+            }
+
+            Console.WriteLine("3- Donde se fundo RedSock?");
+            Console.WriteLine("1. Madrid");
+            Console.WriteLine("2. Andorra");
+            Console.WriteLine("3. Playa de Aro");
+            Console.WriteLine("4. Girona");
+
+            Console.WriteLine("Introdueix el valor: ");
+            rpreg = Convert.ToChar(Console.ReadKey().KeyChar);
+
+            switch (rpreg) 
+            {
+                case '1':
+                    r = r;
+                    break;
+                case '2':
+                    r = r;
+                    break;
+                case '3':
+                    r = r;
+                    break;
+                case '4':
+                    r++;
+                    break;
+            }
+
+            Console.WriteLine("4- Presuspost Inicial?");
+            Console.WriteLine("1. 225€");
+            Console.WriteLine("2. 110€");
+            Console.WriteLine("3. 500€");
+            Console.WriteLine("4. 1212€");
+
+            Console.WriteLine("Introdueix el valor: ");
+            rpreg = Convert.ToChar(Console.ReadKey().KeyChar);
+
+            switch (rpreg)
+            {
+                case '1':
+                    r = r;
+                    break;
+                case '2':
+                    r++;
+                    break;
+                case '3':
+                    r = r;
+                    break;
+                case '4':
+                    r = r;
+                    break;
+            }
+
+            Console.WriteLine("5- En quin any sera la sortida de Glore?");
+            Console.WriteLine("1. 2022");
+            Console.WriteLine("2. 2021");
+            Console.WriteLine("3. 2023");
+            Console.WriteLine("4. 2050");
+
+            Console.WriteLine("Introdueix el valor: ");
+            rpreg = Convert.ToChar(Console.ReadKey().KeyChar);
+
+            switch (rpreg)  //Switch
+            {
+                case '1':
+                    r = r;
+                    break;
+                case '2':
+                    r++;
+                    break;
+                case '3':
+                    r = r;
+                    break;
+                case '4':
+                    r = r;
+                    break;
+            }
+            Console.WriteLine("Errors: " + (5 - r) + ", Correctes: " + r);
         }
 
         static void Tema3()
