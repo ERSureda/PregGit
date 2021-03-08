d31bb3f2e48d9727067075ec1d48c991c441c3b0
Eduard <edurisub@gmail.com>
2021-03-08 13:08:16 +0100

    Agregar archivos de proyecto.

---
+++ PregGit/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PregGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            char mpreg;
+            Console.Clear();
+            Console.WriteLine("\n\tMENU \n");
+            Console.WriteLine("\t\t   1- Matematicas.");
+            Console.WriteLine("\t\t   2- RedSock.");
+            Console.WriteLine("\t\t   3- Mercadona.");
+            Console.WriteLine("\n\t\t    Presionar Q per tencar el programa.");
+
+
+            Console.WriteLine("Introdueix el valor: ");
+            mpreg = Convert.ToChar(Console.ReadKey().KeyChar);
+
+            switch (mpreg)
+            {
+                case '1':
+                    Tema1();
+                    break;
+                case '2':
+                    Tema2();
+                    break;
+                case '3':
+                    Tema3();
+                    break;
+                case 'Q':
+                    break;
+                case 'q':
+                    break;
+            }
+        }
+
+        static void Tema1()
+        {
+
+        }
+
+        static void Tema2()
+        {
+
+        }
+
+        static void Tema3()
+        {
+
+        }
+    }
+}
