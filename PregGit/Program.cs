7ce3ac3a23ad55fe6e5b68f997bb233cef37e94b
Eduard <edurisub@gmail.com>
2021-03-08 19:25:19 +0100

    R3Versio1

--- PregGit/Program.cs
+++ PregGit/Program.cs
@@ -57,7 +57,137 @@ namespace PregGit
 
         static void Tema3()
         {
+            char rpreg;
+            int r = 0;
+            Console.Clear();
+            Console.WriteLine("MERCADONA");
 
+            Console.WriteLine("\t\t\t   1- De donde es Mercadona?");
+            Console.WriteLine("1. Aleman");
+            Console.WriteLine("2. Italiano");
+            Console.WriteLine("3. Frances");
+            Console.WriteLine("4. Español");
+
+            Console.WriteLine("Introdueix el valor: ");
+            rpreg = Convert.ToChar(Console.ReadKey().KeyChar);
+
+            switch (rpreg)
+            {
+                case '1':
+                    r = r;
+                    break;
+                case '2':
+                    r = r;
+                    break;
+                case '3':
+                    r = r;
+                    break;
+                case '4':
+                    r++;
+                    break;
+            }
+
+
+            Console.WriteLine("2- Any de fundació?");
+            Console.WriteLine("1. 1975");
+            Console.WriteLine("2. 2000");
+            Console.WriteLine("3. 1977");
+            Console.WriteLine("4. 1981");
+
+            Console.WriteLine("Introdueix el valor: ");
+            rpreg = Convert.ToChar(Console.ReadKey().KeyChar);
+
+            switch (rpreg)
+            {
+                case '1':
+                    r = r;
+                    break;
+                case '2':
+                    r = r;
+                    break;
+                case '3':
+                    r++;
+                    break;
+                case '4':
+                    r = r;
+                    break;
+            }
+
+            Console.WriteLine("3- El mejor supermercado?");
+            Console.WriteLine("1. Bonpreu");
+            Console.WriteLine("2. Mercadona");
+            Console.WriteLine("3. Suma");
+            Console.WriteLine("4. Spar");
+
+            Console.WriteLine("Introdueix el valor: ");
+            rpreg = Convert.ToChar(Console.ReadKey().KeyChar);
+
+            switch (rpreg)
+            {
+                case '1':
+                    r = r;
+                    break;
+                case '2':
+                    r = r;
+                    break;
+                case '3':
+                    r = r;
+                    break;
+                case '4':
+                    r++;
+                    break;
+            }
+
+            Console.WriteLine("\t\t\t   4- Que color relacionan a mercadona?");
+            Console.WriteLine("1. Rojo");
+            Console.WriteLine("2. verde");
+            Console.WriteLine("3. Marron");
+            Console.WriteLine("4. Azul");
+
+            Console.WriteLine("Introdueix el valor: ");
+            rpreg = Convert.ToChar(Console.ReadKey().KeyChar);
+
+            switch (rpreg)
+            {
+                case '1':
+                    r = r;
+                    break;
+                case '2':
+                    r++;
+                    break;
+                case '3':
+                    r = r;
+                    break;
+                case '4':
+                    r = r;
+                    break;
+            }
+
+            Console.WriteLine("\t\t\t   5- En que pais tiene mas supermercados?");
+            Console.WriteLine("1. Portugal");
+            Console.WriteLine("2. Italia");
+            Console.WriteLine("3. Suiza");
+            Console.WriteLine("4. España");
+
+            Console.WriteLine("Introdueix el valor: ");
+            rpreg = Convert.ToChar(Console.ReadKey().KeyChar);
+
+            switch (rpreg)
+            {
+                case '1':
+                    r = r;
+                    break;
+                case '2':
+                    r = r;
+                    break;
+                case '3':
+                    r = r;
+                    break;
+                case '4':
+                    r++;
+                    break;
+            }
+            Console.WriteLine("Errors: " + (5 - r) + ", Correctes: " + r);
         }
     }
 }
